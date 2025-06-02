038218918413fb4aac3bfc48a549b0c5ca74a8e8
Antoine MARTIN <antoinemartin@MacBookPro-M1-de-Antoine-Martin.local>
2025-06-02 11:52:54 +0200

    first commit

---
+++ src/main/java/com/kata/bankaccountback/KataBankAccountBackApplication.java
@@ -0,0 +1,13 @@
+package com.kata.bankaccountback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBankAccountBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataBankAccountBackApplication.class, args);
+    }
+
+}
