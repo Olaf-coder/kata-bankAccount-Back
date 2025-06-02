c527be66e8ddc4e278c8a0915d7639004b37f115
Antoine MARTIN <antoinemartin@MacBookPro-M1-de-Antoine-Martin.local>
2025-06-02 15:05:01 +0200

    Ajout du mapper

--- src/main/java/com/kata/bankaccountback/domain/mapper/TransactionMapper.java
+++ src/main/java/com/kata/bankaccountback/domain/mapper/TransactionMapper.java
@@ -1,4 +1,26 @@
 package com.kata.bankaccountback.domain.mapper;
 
+import com.kata.bankaccountback.domain.model.dto.TransactionDto;
+import com.kata.bankaccountback.domain.model.entity.TransactionEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransactionMapper {
+    public TransactionDto toDto(TransactionEntity entity) {
+        return new TransactionDto(
+                entity.getId(),
+                entity.getDate(),
+                entity.getDepositAmount(),
+                entity.getWithdrawAmount()
+        );
+    }
+
+    public TransactionEntity toEntity(TransactionDto dto) {
+        TransactionEntity entity = new TransactionEntity();
+        entity.setId(dto.id());
+        entity.setDate(dto.date());
+        entity.setDepositAmount(dto.depositAmount());
+        entity.setWithdrawAmount(dto.withdrawAmount());
+        return entity;
+    }
 }
