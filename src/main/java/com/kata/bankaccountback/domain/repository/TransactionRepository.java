9daed1b41348b1b21d4716cc542e32016f15233b
Antoine MARTIN <antoinemartin@MacBookPro-M1-de-Antoine-Martin.local>
2025-06-02 15:09:32 +0200

    Ajout transactionRepository

--- src/main/java/com/kata/bankaccountback/domain/repository/TransactionRepository.java
+++ src/main/java/com/kata/bankaccountback/domain/repository/TransactionRepository.java
@@ -1,4 +1,16 @@
 package com.kata.bankaccountback.domain.repository;
 
-public class TransactionRepository {
+import com.kata.bankaccountback.domain.model.entity.TransactionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("com.kata.bankaccountback.domain.repository.TransactionRepository")
+public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
+
+    List<TransactionEntity> findAll();
+
+    Optional<TransactionEntity> findById(Long id);
 }
